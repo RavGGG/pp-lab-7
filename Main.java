360fa2a037553b3fda1785d2f9d38574c6aadab0
Rafal <rafalgad@wp.pl>
2024-06-01 15:29:39 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -47,15 +50,16 @@ public class Main extends Application {
     private void browseDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select Directory");
-        java.io.File selectedDirectory = directoryChooser.showDialog(null);
+        File selectedDirectory = directoryChooser.showDialog(null);
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
     }
 
-
     private void searchFiles() {
         String directoryPath = directoryPathField.getText();
+        String searchPhrase = searchField.getText();
+
         if (directoryPath.isEmpty()) {
             resultArea.setText("Please provide a directory path.");
             return;
@@ -68,22 +72,39 @@ public class Main extends Application {
         }
 
         StringBuilder results = new StringBuilder();
-        listFilesInDirectory(directory, results);
+        searchInDirectory(directory, searchPhrase, results);
         resultArea.setText(results.toString());
     }
 
-    private void listFilesInDirectory(File directory, StringBuilder results) {
+    private void searchInDirectory(File directory, String searchPhrase, StringBuilder results) {
         for (File file : directory.listFiles()) {
             if (file.isDirectory()) {
-                listFilesInDirectory(file, results);
+                searchInDirectory(file, searchPhrase, results);
             } else {
-                results.append(file.getAbsolutePath()).append("\n");
+                try {
+                    if (containsPhrase(file, searchPhrase)) {
+                        results.append(file.getAbsolutePath()).append("\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-
-
+    private boolean containsPhrase(File file, String searchPhrase) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(searchPhrase)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            throw new IOException("Error reading file", e);
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         launch(args);
