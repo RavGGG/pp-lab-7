5d9cdaf4798035a9b62d023cf5d88256886958b8
Rafal <rafalgad@wp.pl>
2024-06-01 15:09:56 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -32,6 +34,7 @@ public class Main extends Application {
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
@@ -49,6 +52,38 @@ public class Main extends Application {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
         }
     }
+
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        for (File file : directory.listFiles()) {
+            if (file.isDirectory()) {
+                listFilesInDirectory(file, results);
+            } else {
+                results.append(file.getAbsolutePath()).append("\n");
+            }
+        }
+    }
+
+
+
 
     public static void main(String[] args) {
         launch(args);
